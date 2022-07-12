e37e0ff34b1882108eb2f860a7e4996d8ec52765
alexismcsherry22 <alexismcsherry22@gmail.com>
2022-07-12 23:11:05 +0930

    calculator almost fully functioning, find out why lastMath = 0

--- calculator/main.js
+++ calculator/main.js
@@ -2,7 +2,10 @@ import {calAdd, calSub, calMul, calDiv} from "./math.js";
 
 let calculatorState = false; //default to off
 
-const buttons = document.querySelectorAll(".calculator__buttons");
+let operandOne = 0;
+let operandTwo = 0;
+let lastMath = 0;
+let answer;
 
 const acButton = document.querySelector("#acButton");
 const offButton = document.querySelector("#offButton");
@@ -24,12 +27,20 @@ acButton.addEventListener("click", () =>
   {
     document.getElementById("calculationPara").value = "";
   }
+  operandOne = 0;
+  operandTwo = 0;
+  answer = 0;
+  lastMath = 0;
 });
 
 //turn off
 offButton.addEventListener("click", () =>
 {
   calculatorState === false; //turned off
+  operandOne = 0;
+  operandTwo = 0;
+  answer = 0;
+  lastMath = 0;
   
   document.getElementById("calculationPara").innerHTML = "0000000000";
     
@@ -110,47 +121,108 @@ decimal.addEventListener("click", () => {
 });
 
 //All symbols for calculation
-unique = document.querySelectorAll(".uniqueSymbol");
+const unique = document.querySelectorAll(".uniqueSymbol");
 
-squareRoot = document.querySelector("#sqrButton");
-divide = document.querySelector("#divideButton");
-multiply = document.querySelector("#timesButton");
-percent = document.querySelector("#percentButton");
-subtract = document.querySelector("#minusButton");
-plusMinus = document.querySelector("#plusminusButton");
-addition = document.querySelector("#plusButton");
-equals = document.querySelector("#equalsButton");
+const squareRoot = document.querySelector("#sqrButton");
+const divide = document.querySelector("#divideButton");
+const multiply = document.querySelector("#timesButton");
+const percent = document.querySelector("#percentButton");
+const subtract = document.querySelector("#minusButton");
+const plusMinus = document.querySelector("#plusminusButton");
+const addition = document.querySelector("#plusButton");
+const equals = document.querySelector("#equalsButton");
 
 
-let operandOne = 0;
-let operandTwo = 0;
-
-const convertStrToNum = ((str, math) => {
-  
-  let num = Number(str);
-  console.log(num);
-  if (operandOne > 0)
-  {
+const combineNumbers = ((str, math) => {
+  let num = parseInt(str);
+  if (operandOne > 0 && operandTwo === 0) {
     operandTwo = num;
-    console.log(operandTwo);
+    switch (math) {
+      case math === squareRoot:
+        answer = Math.sqrt(operandOne);
+        break; 
+      case math === percent:
+        operandOne *= .100;
+        break;
+      case math === plusMinus: 
+       // don't understand
+        break;
+      case math === divide:
+        lastMath = divide;
+        break;
+      case math === multiply:
+        lastMath = multiply;
+        break;
+      case math === subtract:
+        lastMath = subtract;
+        break;
+      case math === addition:
+        lastMath = addition;
+        break;
+    }
+  } else if (operandOne > 0 && operandTwo > 0){
+    switch (math) {
+      case math === divide:
+        operandOne = calDiv(operandOne, operandTwo);
+        lastMath = divide;
+        break;
+      case math === multiply:
+        operandOne = calMul(operandOne, operandTwo);
+        lastMath = multiply;
+        break;
+      case math === subtract:
+        operandOne = calSub(operandOne, operandTwo);
+        lastMath = subtract;
+        break;
+      case math === addition:
+        operandOne = calAdd(operandOne, operandTwo);
+        lastMath = addition;
+        console.log(lastMath);
+        break;
+    }
+    operandTwo = 0;
   }
-  else if (operandOne > 0 && operandTwo > 0)
-  {
-
-  }
-  else 
-  {
+  else {
     operandOne = num;
-    console.log(operandOne);
   }
+  if (math === equals) {
+    console.log(lastMath);
+    if (lastMath === divide) {
+      answer = calDiv(operandOne, operandTwo);
+    } 
+    if (lastMath === multiply) {
+      answer = calMul(operandOne, operandTwo);
+    } 
+    if (lastMath === subtract) {
+      answer = calSub(operandOne, operandTwo);
+    } 
+    if (lastMath === addition) {
+      answer = calAdd(operandOne, operandTwo);
+    } 
+    if (lastMath === 0) {
+      answer = "INPUT ERROR";
+    }
+  }
+  if (answer === "INPUT ERROR"){
+    document.getElementById("calculationPara").innerText = answer;
+  }
+  if (answer > 0) {
+    document.getElementById("calculationPara").innerText = answer;
+  } else {
+    document.getElementById("calculationPara").innerText = "";
+  }
+  console.log(operandOne);
+  console.log(operandTwo);
+  console.log(answer);
 })
 
 
-unique.forEach(button => {
+unique.forEach((button) => {
   button.addEventListener("click", () => {
-    let str = document.getElementById("calculationPara").value;
-    let math = document.querySelector(button);
-    convertStrToNum(str, math);
+    console.log(button);
+    let str = document.getElementById("calculationPara").innerText;
+    let math = button;
+    combineNumbers(str, math);
   })
 })
 
